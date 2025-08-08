d7836337752c525b147f81f05c48d2255e035a82
paprockiw <paprockiw@gmail.com>
2025-08-07 22:22:02 -0500

    Fix cluster permissions and reorganize config
    Rewrites some of the config to include enhanced permissions and
    groups resources togetether. Also adds OIDC config to allow EKS to use
    IAM roles within the cluster.

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,21 +1,6 @@
-
-
-#data "aws_availability_zones" "available" {}
-
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster.arn
-
-  vpc_config {
-    subnet_ids = var.eks_subnets
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
-}
-
+# EKS Cluster IAM Role
 resource "aws_iam_role" "eks_cluster" {
   name = "eks-cluster-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -28,12 +13,30 @@ resource "aws_iam_role" "eks_cluster" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-data "aws_eks_cluster_auth" "cluster" {
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.29"
+
+  vpc_config {
+    subnet_ids = var.eks_subnets
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+data "aws_eks_cluster" "eks" {
   name = aws_eks_cluster.eks.name
 }
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+
 
--- fargate_profile.tf
+++ fargate_profile.tf
@@ -1,3 +1,23 @@
+resource "aws_iam_role" "fargate_execution_role" {
+  name = "eks-fargate-pod-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_execution" {
+  role       = aws_iam_role.fargate_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
 resource "aws_eks_fargate_profile" "fargate_prf" {
   cluster_name           = aws_eks_cluster.eks.name
   fargate_profile_name   = "web-profile"
@@ -7,5 +27,12 @@ resource "aws_eks_fargate_profile" "fargate_prf" {
   selector {
     namespace = "web"
   }
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.fargate_execution
+  ]
 }
+
+
 
